5ec38dece1f9102ad429bafe959e64c043561f1d
Husein Avicenna <huseinavicenna5@gmail.com>
2026-06-04 16:11:18 +0700

    Pertemuan 11

---
+++ PBO11_24060124120047/src/main/java/com/mycompany/pertemuan11/Pertemuan11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan11;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pertemuan11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
