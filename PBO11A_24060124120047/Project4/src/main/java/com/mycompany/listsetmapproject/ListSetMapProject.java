9b19610953b49a9b731e48e359f9c0ee58c95f30
Husein Avicenna <huseinavicenna5@gmail.com>
2026-06-04 17:50:32 +0700

    Pertemuan 11A

---
+++ PBO11A_24060124120047/Project4/src/main/java/com/mycompany/listsetmapproject/ListSetMapProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listsetmapproject;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ListSetMapProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
